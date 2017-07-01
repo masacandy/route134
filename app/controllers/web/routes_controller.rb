075b6b75b5cc6402a1a0e158c933ab7460213904
Masanobu Mizutani <James@Masanobus-MacBook-Air.local>
2017-07-01 23:29:47 +0900

    markersをメソッドに分ける

--- app/controllers/web/routes_controller.rb
+++ app/controllers/web/routes_controller.rb
@@ -3,8 +3,15 @@ class Web::RoutesController < Web::BaseController
 
   def show
     route = Route.find(params[:id])
+
     @spots = route.spots
-    @hash = Gmaps4rails.build_markers(@spots) do |spot, maker|
+    @hash = create_markers
+  end
+
+  private
+
+  def create_markers
+    Gmaps4rails.build_markers(@spots) do |spot, maker|
       maker.lat spot.latitude.to_f
       maker.lng spot.longtitude.to_f
     end
