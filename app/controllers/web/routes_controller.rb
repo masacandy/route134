6aba6a36e8057430191e259bdd116abcda85a21e
Masanobu Mizutani <James@Masanobus-MacBook-Air.local>
2017-07-01 18:06:34 +0900

    mapのmarkerをdbから取得

--- app/controllers/web/routes_controller.rb
+++ app/controllers/web/routes_controller.rb
@@ -1,9 +1,12 @@
 class Web::RoutesController < Web::BaseController
-  def index
-  end
+  def index;end
 
   def show
     route = Route.find(params[:id])
     @spots = route.spots
+    @hash = Gmaps4rails.build_markers(@spots) do |spot, maker|
+      maker.lat spot.latitude.to_f
+      maker.lng spot.longtitude.to_f
+    end
   end
 end
