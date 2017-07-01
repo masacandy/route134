60660dbcbb848bec00a2c174321127e40ff0cea8
Masanobu Mizutani <James@Masanobus-MacBook-Air.local>
2017-07-01 15:44:04 +0900

    added show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope module: :web, defaults: { format: :html } do
     root to: 'top#index'
-    resources :routes, only: %i(index)
+    resources :routes, only: %i(index show)
   end
 end
