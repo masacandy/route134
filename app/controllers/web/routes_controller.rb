5f6de98b85a6ca80b31477e5d5e84698770d163f
Masanobu Mizutani <James@Masanobus-MacBook-Air.local>
2017-07-13 23:06:52 +0900

    マーカーを色分けするように修正

--- app/controllers/web/routes_controller.rb
+++ app/controllers/web/routes_controller.rb
@@ -21,9 +21,14 @@ class Web::RoutesController < Web::BaseController
   end
 
   def create_markers
-    Gmaps4rails.build_markers(@route.spots) do |spot, maker|
-      maker.lat spot.latitude.to_f
-      maker.lng spot.longtitude.to_f
+    Gmaps4rails.build_markers(@route.route_spots) do |route_spot, maker|
+      maker.lat route_spot.spot.latitude.to_f
+      maker.lng route_spot.spot.longtitude.to_f
+      maker.picture({
+        url: ActionController::Base.helpers.asset_path("markers/#{route_spot.sort}.png"),
+        width: 32,
+        height: 32
+      })
     end
   end
 end
